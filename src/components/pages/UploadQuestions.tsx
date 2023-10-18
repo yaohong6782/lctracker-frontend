afcc95f81e8d5785eb61ab7a665b59c47016a019
yaohong6782 <yaohong6782@gmail.com>
2023-10-19 03:34:30 +0800

    post question frontend

--- src/components/pages/UploadQuestions.tsx
+++ src/components/pages/UploadQuestions.tsx
@@ -15,6 +15,9 @@ import { UploadSchema } from "@/zodSchema/upload";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
 const UploadQuestions = () => {
   const form = useForm<z.infer<typeof UploadSchema>>({
     resolver: zodResolver(UploadSchema),
@@ -32,24 +35,27 @@ const UploadQuestions = () => {
   };
   return (
     <>
+      <br />
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-4 grid justify-center"
         >
-          <h2 className="text-2xl underline flex justify-center">Upload</h2>
+          <h2 className="text-2xl underline flex justify-center">
+            Post Question
+          </h2>
           <FormField
             control={form.control}
             name="questionNumber"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="flex justify-center">
+                <FormLabel>
                   Question Number
                 </FormLabel>
                 <FormControl className="lg:w-96 flex items-center justify-content-center md:w-72">
                   <Input type="text" placeholder="Question Number" {...field} />
                 </FormControl>
-                <FormDescription>asdf</FormDescription>
+                <FormDescription>E.g 1803</FormDescription>
                 <FormMessage />
               </FormItem>
             )}
@@ -60,7 +66,7 @@ const UploadQuestions = () => {
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Title </FormLabel>
-                <FormControl>
+                <FormControl className="lg:w-96 flex items-center justify-content-center md:w-72">
                   <Input
                     type="text"
                     placeholder="Title of problem"
@@ -71,6 +77,40 @@ const UploadQuestions = () => {
               </FormItem>
             )}
           />
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Problem Link </FormLabel>
+                <FormControl className="lg:w-96 flex items-center justify-content-center md:w-72">
+                  <Input type="text" placeholder="Link" {...field} />
+                </FormControl>
+                <FormDescription className="lg:w-96 flex items-center justify-content-center md:w-72">
+                  E.g <br />
+                  https://leetcode.com/problems/unique-binary-search-trees/description/
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Solution / Intuition</FormLabel>
+                <FormControl className="lg:w-96 flex items-center justify-content-center md:w-72">
+                  <Textarea
+                    placeholder="Solution / Intuition to Problem"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button>Post</Button>
         </form>
       </Form>
     </>
--- src/zodSchema/upload.ts
+++ src/zodSchema/upload.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 
 export const UploadSchema = z.object({
-    questionNumber : z.string().min(1, {message: "Please enter the question number"}),
-    title: z.string().min(3, {message : "Minimum character of 3 characters for Title"}),
-    url : z.string().min(1, {message : "Please enter a link"}),
-    content : z.string().min(1, {message : "Please enter a content"})
-  });
-  
+  questionNumber: z
+    .string()
+    .min(1, { message: "Please enter the question number" }),
+  title: z
+    .string()
+    .min(3, { message: "Minimum character of 3 characters for Title" }),
+  url: z.string().url({message : "Please enter a valid URL containing http"}).min(1, { message: "Please enter a link" }),
+  content: z.string().min(1, { message: "Please enter a solution" }),
+});
